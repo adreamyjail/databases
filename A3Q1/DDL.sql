06f3830300e8d447e7f50a9f3d26d5dc18202525
adreamyjail <137181434+adreamyjail@users.noreply.github.com>
2024-03-18 06:48:21 -0400

    email attribute NOT NULL, UNIQUE*

--- A3Q1/DDL.sql
+++ A3Q1/DDL.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS students (
     student_id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     enrollment_date DATE DEFAULT CURRENT_DATE
 );
