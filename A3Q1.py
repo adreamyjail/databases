000964b2344624a369865b366599c584077848c9
adreamyjail <137181434+adreamyjail@users.noreply.github.com>
2024-03-18 06:28:08 -0400

    Refactored database connection: global, user input

--- A3Q1.py
+++ A3Q1.py
@@ -4,34 +4,42 @@
 import sys
 import psycopg
 
-def connectToDB(dbname = "a3p1", user = "ta", password = "greetings", host = "localhost", port = 5432):
-    db = {
-        "dbname": dbname,
-        "user": user,
-        "password": password,
-        "host": host,
-        "port": port
-    }
-    
-    try:
-        print("\nAttempting to connect to PostgreSQL database...")
-        conn = psycopg.connect(**db)
-    except psycopg.DatabaseError as e:
-        print("Unable to connect to the database. Exiting...")
-        print(f"Error: {e}")
-        sys.exit(1)
+conn = None
 
-    successful_connection = f"Connected to the PostgreSQL database: '{dbname}' on {host}:{port}"
-    print(successful_connection)
-    print(f"{'-' * len(successful_connection)}\n")
+def connectToDB():
+    global conn
+    if conn is None:
+        dbname = input("Enter the database name: ") or "a3p1"
+        password = input("Enter the database password: ") or "greetings"
+        user = input("Enter the database user: ") or "ta"
+        host = input("Enter the database host: ") or "localhost"
+        port = input("Enter the database port: ") or "5432"
+            
+        try:
+            print("\nAttempting to connect to PostgreSQL database...")
+            db = {
+                "dbname": dbname,
+                "user": user,
+                "password": password,
+                "host": host,
+                "port": port
+            }
+            conn = psycopg.connect(**db)
+        except psycopg.DatabaseError as e:
+            print("Unable to connect to the database. Exiting...")
+            print(f"Error: {e}")
+            sys.exit(1)
+
+        successful_connection = f"Connected to the PostgreSQL database: '{dbname}' on {host}:{port}"
+        print(successful_connection)
+        print(f"{'-' * len(successful_connection)}\n")
     return conn
                                   
-def formatResultStrings(first_name: str, last_name: str, email: str):
+def formatResultStrings(first_name: str, last_name: str, email: str) -> tuple[str, str, str]:
     '''
     HELPER FUNCTION (getAllStudents())
-    Formats first_name last_name and email strings such that they fit
-    within the specified column widths in the output table. The extra
-    characters are replaced with "..." to indicate string truncation.
+    Formats first_name, last_name, and email strings such that they fit within the specified column 
+    widths in the output table. Extra characters are replaced with "..." to indicate string truncation.
     '''
     if len(first_name) > 20:
         first_name = first_name[:17] + "..."
@@ -46,7 +54,6 @@ def getAllStudents():
     FROM ASSIGNMENT SPECIFICATION:
     Retrieves and displays all records from the students table
     '''
-    conn = connectToDB()
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM students ORDER BY student_id ASC;")
         rows = cur.fetchall()
@@ -63,47 +70,47 @@ def addStudent(first_name: str, last_name: str, email: str, enrollment_date: str
     FROM ASSIGNMENT SPECIFICATION: 
     Inserts a new student record into the students table
     '''
-    conn = connectToDB()
     with conn.cursor() as cur:
         cur.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);",
                     (first_name, last_name, email, enrollment_date))
         conn.commit()
-        print(f"Added student:\n-- Name: {first_name} {last_name}\n-- Email: {email}\n-- Enrollment date: '{enrollment_date}'\n")
+        print(f"Added student to the database.\n++ Name: {first_name} {last_name}\n++ Email: {email}\n++ Enrollment date: '{enrollment_date}'\n")
 
 def updateStudentEmail(student_id: int, email: str):
     '''
     FROM ASSIGNMENT SPECIFICATION: 
     Updates the email address for a student with the specified student_id
     '''
-    conn = connectToDB()
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM students WHERE student_id = %s", (student_id,))
         row = cur.fetchone()
         if row is None:
             print(f"Invalid input. No student with ID '{student_id}' exists.\n")
             return
+        student_id, first_name, last_name, old_email, _ = row
         cur.execute("UPDATE students SET email = %s WHERE student_id = %s", (email, student_id))
         conn.commit()
-        print(f"Updated email for student with id: {student_id}.\n")
+        print(f"Updated email for Student ID {student_id} from ...\n   [OLD] {old_email} -> \n   [NEW] {email}\n")
     
 def deleteStudent(student_id: int):
     '''
     FROM ASSIGNMENT SPECIFICATION: 
     Deletes the record of the student with the specified student_id
     '''
-    conn = connectToDB()
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM students WHERE student_id = %s", (student_id,))
         row = cur.fetchone()
         if row is None:
             print(f"Invalid input. No student with ID '{student_id}' exists.\n")
             return
+        student_id, first_name, last_name, email, enrollment_date = row
+        print(f"Deleted student from the database.\n-- Name: {first_name} {last_name}\n-- Email: {email}\n-- Enrollment date: '{enrollment_date}'\n")
         cur.execute("DELETE FROM students WHERE student_id = %s", (student_id,))
         conn.commit()
-        print(f"Deleted student with id: {student_id}\n")
 
-def main():   
-    user_input = input("Choose an option:\n  [1]. Display all students\n  [2]. Add a student\n  [3]. Update a student's email\n  [4]. Delete a student via student ID\n  [0]. Exit\n>>>")
+def main():
+    connectToDB()
+    user_input = input("Choose an option:\n  [1] Display all students\n  [2] Add a student\n  [3] Update a student's email\n  [4] Delete a student via student ID\n  [0] Exit\n>>>")
     match user_input:
         case '0':
             print("Exiting...\n")
